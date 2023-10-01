9cf2a7448402ce57f32be70e23897cac95228c41
Ayxa Chaverra <achaverrar@gmail.com>
2023-10-01 12:13:23 -0500

    move the Alert component to the Registro one to match the requirements

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import Registro from './componet/Registro'
-import Formulario from './componet/Formulario'
-import Alert from './componet/Alert'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Registro from "./componet/Registro";
+import Formulario from "./componet/Formulario";
+import Alert from "./componet/Alert";
 
 function App() {
-  const [mensaje, setMensaje] = useState('');
-  const [color, setColor] = useState('');
+  const [mensaje, setMensaje] = useState("");
+  const [color, setColor] = useState("");
 
   const RegistroExitoso = () => {
     setMensaje("¡Registro exitoso!");
     setColor("success");
   };
-
 
   const CamposVacios = () => {
     setMensaje("Por favor, completa todos los campos.");
@@ -27,9 +26,10 @@ function App() {
       <Registro
         RegistroExit={RegistroExitoso}
         CamposVacio={CamposVacios}
+        mensaje={mensaje}
+        color={color}
       />
-      <Alert mensaje={mensaje} color={color} />
     </div>
   );
 }
-export default App
+export default App;
